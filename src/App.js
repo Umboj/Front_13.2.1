bc0da2b47a67d93889e078dfa1e069ce0866306c
Wojtek <umboj2000@gmail.com>
2023-06-06 13:44:10 +0200

    cale zadanie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Home = () => (<h1>Nie znaleziono elementu '/'</h1>)
+const About = () => (<h1>Nie znaleziono elementu '/About'</h1>)
+const Services = () => (<h1>Nie znaleziono elementu '/Services'</h1>)
+const Contact = () => (<h1>Nie znaleziono elementu '/Contact'</h1>)
+const ContactUs = () => (<h1>Nie znaleziono elementu '/Contact/Us'</h1>)
+const ContactPl = () => (<h1>Nie znaleziono elementu '/Contact/Pl'</h1>)
+const ContactDe = () => (<h1>Nie znaleziono elementu '/Contact/De'</h1>)
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact/us" element={<ContactUs />} />
+          <Route path="/contact/pl" element={<ContactPl />} />
+          <Route path="/contact/de" element={<ContactDe />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
